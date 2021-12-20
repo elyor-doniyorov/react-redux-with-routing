bec9b5e7c76af2853b4afdd86e60af455d0b5492
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-12-21 00:43:35 +0500

    Fetch api and install axios

--- src/containers/HomePage/index.jsx
+++ src/containers/HomePage/index.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 import { createSelector } from 'reselect';
 import { makeSelectUsers } from './selectors';
+import Axios from 'axios';
+import { useEffect } from 'react';
 
 
 const stateSelector = createSelector(makeSelectUsers, (users) => ({
@@ -10,6 +12,15 @@ const stateSelector = createSelector(makeSelectUsers, (users) => ({
 
 export function HomePage(props) {
   const {users} = useSelector(stateSelector);
-  console.log("Users: ", users);
+  const fetchUsers = async () => {
+    const response = await Axios.get('https://reqres.in/api/users').catch((err) => {
+      console.log('Err: ', err);
+    })
+    console.log('Users: ', response.data.data)
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
   return <div>Hello World!</div>
 }
--- src/containers/HomePage/reducers.js
+++ src/containers/HomePage/reducers.js
@@ -1,5 +1,5 @@
 const defaultState = {
-  users: ["No User!"],
+  users: [],
 };
 
 export default function homePageReducer(state = defaultState, action) {
