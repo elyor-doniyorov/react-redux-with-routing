0df2a5a22118b46d728e85eebd5bf1270dcb02ca
Elyor Doniyorov <doniyorovelyor@gmail.com>
2021-12-20 13:13:24 +0500

    Import and use Provider to connect react vs redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { HomePage } from './containers/HomePage/index';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello React Lovers!</h1>
+      <h1><HomePage /></h1>
     </div>
   );
 }
